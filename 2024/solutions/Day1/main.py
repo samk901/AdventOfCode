65a3fc7a8cee1296470ddad696af26cd51cea90a
Sam Kim <samk90@gmail.com>
2024-12-02 13:19:21 -0500

    Day1 Part B

--- 2024/solutions/Day1/main.py
+++ 2024/solutions/Day1/main.py
@@ -11,7 +11,7 @@ from utils.file_utils import FileUtils
 def main():
 
     global lines 
-    lines = FileUtils.read_file_as_lines("/home/sam/Desktop/AdventOfCode/2024/input/Day1_Test")
+    lines = FileUtils.read_file_as_lines("/home/sam/Desktop/AdventOfCode/2024/input/Day1")
 
     PartA()
     PartB()
@@ -48,23 +48,28 @@ def PartB():
 
     # Need to count the number of instances in order to process
     # Use a hashmap (dict) of value : occurences
-    count_map1 = {}
-    count_map2 = {}
-
+    
+    list1 = []
+    list2 = []
+    list2_count ={}
     total = 0
 
     for line in lines:
-        values = line.split()
-        count_map1[values[0]] = count_map1.get(values[0], 0) + 1
-        count_map2[values[1]] = count_map2.get(values[1], 0) + 1
+        values = line.split("   ")
+        list1.append(values[0])
+        # print(str(line[0]))
+        list2.append(values[1])
+        # print(str(line[1]))
 
-    for key, val in count_map1.items():
-        # print(f"key: {key} value: {val}")
-        if key in count_map2.keys():
-            print(f"the value {key} is in both maps")
-            common = min(count_map1[key], count_map2[key])
-            total += common * int(key)  
-
+        if not (values[1] in list2_count):
+            list2_count[values[1]] = 1
+        else:
+            list2_count[values[1]] += 1
+    
+    for num in list1:
+        if num in list2:
+            total += int(num) * list2_count[num]
+    
     print("++++++++++++++++++++++++++++++++++++")
     print(f"PART B: The total is {total}")
     print("++++++++++++++++++++++++++++++++++++")
